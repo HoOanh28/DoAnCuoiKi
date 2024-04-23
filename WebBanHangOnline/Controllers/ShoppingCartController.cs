fefd33d830ff21d0be1f51b28a3418524eb9cbe6
thuong <tn58213@gmail.com>
2024-04-23 12:04:53 +0700

    oanh

---
+++ WebBanHangOnline/Controllers/ShoppingCartController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+
+namespace WebBanHangOnline.Controllers
+{
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: ShoppingCart
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ShoppingCartController()
+        {
+        }
+
+        public ShoppingCartController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null && cart.Items.Any())
+            {
+                ViewBag.CheckCart = cart;
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Partial_Item_Cart()
+        {
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null && cart.Items.Any())
+            {
+                return PartialView(cart.Items);
+            }
+            return PartialView();
+        }
+
+        [AllowAnonymous]
+        public ActionResult ShowCount()
+        {
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null)
+            {
+                return Json(new { Count = cart.Items.Count }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { Count = 0 }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
